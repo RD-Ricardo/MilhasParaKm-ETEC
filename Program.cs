f32de2de294e9e494703a4e6abf50609b236cf43
RD-Ricardo <ricardodias169@gmail.com>
2021-03-19 14:10:37 -0300

    Finalizando Programa

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MilhasParaKm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 ,num2 ,resp;
+            Console.WriteLine("Milhas Para KM");
+            Console.WriteLine("--------------");
+            Console.Write("Entre com a medida (em milhas): ");
+            num1= Convert.ToDouble(Console.ReadLine());
+            num2 = 1.609;
+            resp = num1 * num2;
+            Console.WriteLine(resp);
+        }
+    }
+}
